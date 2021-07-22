668ff5d0cf5cf2df1eaff4b6f298679e2caf4056
lucas <liquiitass@gmail.com>
2021-07-22 02:05:32 -0300

    url lin reset password

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = 'http://futbol8alem.com/#/password-reset/'. $token . '/'.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Click para restaurar su contraseña: <a href="{{ $link = 'http://futbol8alem.com/#/password-reset/'. $token . '/'.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>

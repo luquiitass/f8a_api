f6b155a735a748b29e7ff9e1ab681fb1e1a08159
lucas <liquiitass@gmail.com>
2021-10-06 11:08:23 -0300

    añadiendo loading

--- resources/views/share/result2.blade.php
+++ resources/views/share/result2.blade.php
@@ -108,6 +108,17 @@
             font-family: none;
         }
 
+        .se-pre-con {
+            display: none;
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url("<?php echo asset('assets/preloader.gif'); ?>" ) left top no-repeat #fff;
+        }
+
     </style>
 
 </head>
